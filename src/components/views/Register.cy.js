0d411bbea540d2c13573dfaff6683c9ae62e7fbe
ruiruiwang <wangruirui45@outlook.com>
2023-05-21 17:34:14 +0200

    fixed problems

--- src/components/views/Register.cy.js
+++ src/components/views/Register.cy.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import Register from './Register'
+import React from 'react';
+import Register from './Register';
 
 describe('<Register />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Register />)
-  })
-})
+// see: https://on.cypress.io/mounting-react
+    cy.mount(<Register />);
+  });
+
+  it('registers a new user', () => {
+    const mockUser = {
+      id: 'mockUserId',
+      token: 'mockToken'
+    };
+    cy.intercept('POST', '/users', {
+      statusCode: 200,
+      body: mockUser
+    }).as('registerRequest');
+
+    cy.mount(<Register />);
+
+    cy.contains('Register').click();
+
+  });
+});
